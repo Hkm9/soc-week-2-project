8a62481967b028027a6d943e3611542e0fc6b07d
Hakim <hakim.mame96@gmail.com>
2020-03-03 12:11:15 +0100

    solutions

--- challenges/pad-left.js
+++ challenges/pad-left.js
@@ -2,6 +2,19 @@ console.log('padLefts script has loaded');
 
 // write this function!
 function padLeft(string, length) {
+  //strubg,padstrat()
+  if (string.length >= length) {
+    return string;
+  }
+  else {
+    let extraDashs = string.length;
+    extraDashs = length - extraDashs;
+    for (i = 0; i < extraDashs; i++) {
+      string = '-' + string;
+
+    }
+    return string;
+  }
 }
 
 // if the string is already as long as or longer than the length
@@ -26,7 +39,8 @@ function padLeftHandler() {
 
   // perform core logic
   // write this line!
-
+  let result = padLeft(userString, userLengthStr);
+  alert(result);
   // alert result for the user
   // write this line!
 
--- challenges/temp-converter.js
+++ challenges/temp-converter.js
@@ -1,18 +1,31 @@
 console.log('tempConverter script has loaded');
 
 // write this function!
-function tempConverter(temperature, degrees) {
-}
+function tempConverter(degrees, temperature) {
+  var resultValue = 0;
+  if (temperature === 'f' || temperature === 'F') {
+    resultValue = degrees * 9 / 5 + 32;
 
+    return resultValue;
+  }
+  else if (temperature === 'c' || temperature === 'C') {
+    resultValue = (degrees - 32) * 5 / 9;
+    return Number(resultValue.toFixed(2));
+  }
+  else {
+    return temperature + " " + "is not supported";
+  }
+
+}
 // if the user wants to convert to farenheit
-console.assert(tempConverter(53, 'farenheit') === 127.4, 'first');
-console.assert(tempConverter(0, 'farenheit') === 32, 'second');
-console.assert(tempConverter(-40, 'farenheit') === -40, 'third');
+console.assert(tempConverter(53, 'f') === 127.4, 'first');
+console.assert(tempConverter(0, 'f') === 32, 'second');
+console.assert(tempConverter(-40, 'f') === -40, 'third');
 
 // if the user wants to convert to celcius
-console.assert(tempConverter(0, 'celcius') === 32, 'fourth');
-console.assert(tempConverter(4, 'celcius') === 39.2, 'fifth');
-console.assert(tempConverter(12.4, 'celcius') === 54.32, 'sixth');
+console.assert(tempConverter(0, 'c') === -17.78, 'fourth');
+console.assert(tempConverter(4, 'c') === -15.56, 'fifth');
+console.assert(tempConverter(12.4, 'c') === -10.89, 'sixth');
 
 // if the user inputs an invalid degree
 console.assert(tempConverter(34, 'toad') === 'toad is not supported', 'seventh');
@@ -21,18 +34,26 @@ console.assert(tempConverter(500, 'Farenheit') === 'Farenheit is not supported',
 
 
 function tempConverterHandler() {
-  // prompt the user for a noun, verb and adjective
-  const userTempStr = prompt('enter a temperature to convert');
-  // cast userDegreesStr to a Number, and assign the value to userDecrees
-  const userDegrees = prompt('would you like to convert to farenheit or celcius?');
-
-  console.assert(typeof userDegrees === 'number', "don't forget to cast userDegrees to a string!");
-
   // perform core logic
   // write this line!
+  let result;
+  // prompt the user for a noun, verb and adjective
+  const userTempStr = prompt('would you like to convert to "f" for farenheit or "c" celcius? , please write "f" or "c" only!');
+  if (String(userTempStr) === 'f' || String(userTempStr) === 'c' || String(userTempStr) === 'C' || String(userTempStr) === 'F') {
+    // cast userDegreesStr to a Number, and assign the value to userDecrees
+    const userDegrees = prompt('enter a temperature to convert');
+    if (Number(userDegrees)) {
+      result = tempConverter(userDegrees, userTempStr);
+      alert(result);
+    } else {
+      alert('Write a valid number please .try again!');
+      console.assert(typeof userDegrees === 'number', "don't forget to cast userDegrees to a string!");
+    }
+  }
+  else
+    alert('Write a valid convert choice please, try again !');
 
-  // alert result for the user
-  // write this line!
+  //alert(result);
 
   // log action for the developer
   console.log('\n--- tempConverter ---');
